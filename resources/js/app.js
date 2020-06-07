ee9489b36fb16e8537bd451a7bbf9de07db63c1d
Andrey Budiharja <andreybudiharja4@gmail.com>
2020-06-07 20:15:56 +0700

    fresh project import dependencies

---
+++ resources/js/app.js
@@ -0,0 +1,22 @@
+// global variable for require js
+require('./bootstrap');
+
+// admin lte dependencies
+require('popper.js');
+require('jquery-ui');
+require('jquery-validation');
+require('moment'); // number formatter
+require('filterizr'); // image gallery
+
+// datatable
+require('jszip');
+require('pdfmake');
+require('datatables.net-bs4');
+require('datatables.net-buttons-bs4');
+require('datatables.net-buttons/js/buttons.html5.js');
+require('datatables.net-buttons/js/buttons.print.js');
+require('datatables.net-responsive-bs4');
+require('datatables.net-searchpanes-bs4');
+require('datatables.net-select-bs4');
+
+require('admin-lte');
